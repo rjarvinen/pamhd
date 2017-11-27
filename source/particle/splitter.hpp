f6af19f6fa30b179d854a69f11a164af65e3bbca
Ilja Honkonen <ilja.honkonen@fmi.fi>
2017-11-27 14:01:12 +0200

    Preserve center of mass when splitting particle into two.

--- source/particle/splitter.hpp
+++ source/particle/splitter.hpp
@@ -55,7 +55,7 @@ namespace particle {
 /*
 Splits random particles in normal cells until given minimum exist.
 
-One split does not conserve particle's center of mass.
+Every split preserves center of mass, other parameters also unchanged.
 */
 template<
 	class Cell,
@@ -72,6 +72,8 @@ template<
 	const Particle_Mass_Getter Part_Mas,
 	const Solver_Info_Getter Sol_Info
 ) {
+	using std::min;
+
 	for (const auto& cell: grid.cells) {
 		if (Sol_Info(*cell.data) != pamhd::particle::Solver_Info::normal) {
 			continue;
@@ -85,27 +87,37 @@ template<
 		const auto
 			cell_min = grid.geometry.get_min(cell.id),
 			cell_max = grid.geometry.get_max(cell.id);
-		std::uniform_real_distribution<double>
-			pos_x_gen(cell_min[0], cell_max[0]),
-			pos_y_gen(cell_min[1], cell_max[1]),
-			pos_z_gen(cell_min[2], cell_max[2]);
 
 		std::uniform_int_distribution<size_t> index_generator(0, original_nr_particles - 1);
 		for (size_t i = 0; i < min_particles - original_nr_particles; i++) {
 			//const auto part_i = index_generator(random_source);
 			auto& particle = Particles(*cell.data)[index_generator(random_source)];
+			const auto old_pos = Part_Pos(particle);
+
+			// particle must be created within this distance in every
+			// coord to stay within cell while preserving center of mass
+			const auto max_distance =
+				min(min(cell_max[0] - old_pos[0], old_pos[0] - cell_min[0]), // x
+				min(min(cell_max[1] - old_pos[1], old_pos[1] - cell_min[1]), // y
+				    min(cell_max[2] - old_pos[2], old_pos[2] - cell_min[2]))); // z
+
+			std::uniform_real_distribution<double> offset_gen(-max_distance, max_distance);
+			const auto
+				offset_x = offset_gen(random_source),
+				offset_y = offset_gen(random_source),
+				offset_z = offset_gen(random_source);
 
 			Part_Mas(particle) /= 2;
-			Part_Pos(particle) = {
-				pos_x_gen(random_source),
-				pos_y_gen(random_source),
-				pos_z_gen(random_source)
-			};
 			Particles(*cell.data).push_back(particle);
 			Part_Pos(particle) = {
-				pos_x_gen(random_source),
-				pos_y_gen(random_source),
-				pos_z_gen(random_source)
+				old_pos[0] + offset_x,
+				old_pos[1] + offset_y,
+				old_pos[2] + offset_z
+			};
+			Part_Pos(Particles(*cell.data)[Particles(*cell.data).size() - 1]) = {
+				old_pos[0] - offset_x,
+				old_pos[1] - offset_y,
+				old_pos[2] - offset_z
 			};
 		}
 	}
