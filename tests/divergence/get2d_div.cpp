0972de1aaa886a8ca9c939f2edf032f5674242c1
Ilja Honkonen <ilja.honkonen@fmi.fi>
2018-11-26 12:44:11 +0200

    Fix compilation of 2d div test.

--- tests/divergence/get2d_div.cpp
+++ tests/divergence/get2d_div.cpp
@@ -56,7 +56,7 @@ double div_of_function(const double x, const double y)
 }
 
 
-struct Vector_Field {
+struct Vector {
 	using data_type = std::array<double, 3>;
 };
 
@@ -64,44 +64,44 @@ struct Divergence {
 	using data_type = double;
 };
 
+struct Type {
+	using data_type = int;
+};
+
 using Cell = gensimcell::Cell<
 	gensimcell::Always_Transfer,
-	Vector_Field,
-	Divergence
+	Vector,
+	Divergence,
+	Type
 >;
 
 
 /*!
 Returns maximum norm if p == 0
 */
-template<class Grid_T> double get_diff_lp_norm(
-	const std::vector<uint64_t>& cells,
-	const Grid_T& grid,
+template<class Grid> double get_diff_lp_norm(
+	const Grid& grid,
 	const double p,
 	const double cell_volume
 ) {
 	double local_norm = 0, global_norm = 0;
-	for (const auto& cell: cells) {
-		const auto* const cell_data = grid[cell];
-		if (cell_data == NULL) {
-			std::cerr << __FILE__ << ":" << __LINE__
-				<< ": No data for cell " << cell
-				<< std::endl;
-			abort();
+	for (const auto& cell: grid.local_cells) {
+		if ((*cell.data)[Type()] != 1) {
+			continue;
 		}
 
-		const auto center = grid.geometry.get_center(cell);
+		const auto center = grid.geometry.get_center(cell.id);
 
 		const auto div_of = div_of_function(center[0], center[1]);
 
 		if (p == 0) {
 			local_norm = std::max(
 				local_norm,
-				std::fabs((*cell_data)[Divergence()] - div_of)
+				std::fabs((*cell.data)[Divergence()] - div_of)
 			);
 		} else {
 			local_norm += std::pow(
-				std::fabs((*cell_data)[Divergence()] - div_of),
+				std::fabs((*cell.data)[Divergence()] - div_of),
 				p
 			);
 		}
@@ -160,23 +160,13 @@ int main(int argc, char* argv[])
 
 		const std::array<uint64_t, 3> grid_size{{nr_of_cells + 2, nr_of_cells + 2, 1}};
 
-		if (
-			not grid.initialize(
-				grid_size,
-				comm,
-				"RANDOM",
-				neighborhood_size,
-				max_refinement_level,
-				false,
-				false,
-				false
-			)
-		) {
-			std::cerr << __FILE__ << ":" << __LINE__
-				<< ": Couldn't initialize grid."
-				<< std::endl;
-			abort();
-		}
+		grid
+			.set_load_balancing_method("RANDOM")
+			.set_initial_length(grid_size)
+			.set_neighborhood_length(neighborhood_size)
+			.set_maximum_refinement_level(max_refinement_level)
+			.initialize(comm)
+			.balance_load();
 
 		const std::array<double, 3>
 			cell_length{{
@@ -192,62 +182,49 @@ int main(int argc, char* argv[])
 		dccrg::Cartesian_Geometry::Parameters geom_params;
 		geom_params.start = grid_start;
 		geom_params.level_0_cell_length = cell_length;
+		grid.set_geometry(geom_params);
 
-		if (not grid.set_geometry(geom_params)) {
-			std::cerr << __FILE__ << ":" << __LINE__
-				<< ": Couldn't set grid geometry."
-				<< std::endl;
-			abort();
-		}
+		for (const auto& cell: grid.local_cells) {
+			const auto center = grid.geometry.get_center(cell.id);
 
-		grid.balance_load();
-
-		const auto all_cells = grid.get_cells();
-		for (const auto& cell: all_cells) {
-			auto* const cell_data = grid[cell];
-			if (cell_data == NULL) {
-				std::cerr << __FILE__ << ":" << __LINE__
-					<< ": No data for cell " << cell
-					<< std::endl;
-				abort();
-			}
-
-			const auto center = grid.geometry.get_center(cell);
-
-			auto& vec = (*cell_data)[Vector_Field()];
+			auto& vec = (*cell.data)[Vector()];
 			vec[0] = 0;
 			vec[1] = function(center[0], center[1]);
 			vec[2] = 0;
 		}
 		grid.update_copies_of_remote_neighbors();
 
-		std::vector<uint64_t> solve_cells;
-		for (const auto& cell: all_cells) {
-			const auto index = grid.mapping.get_indices(cell);
+		for (const auto& cell: grid.local_cells) {
+			const auto index = grid.mapping.get_indices(cell.id);
 			if (
 				index[0] > 0
 				and index[0] < grid_size[0] - 1
 				and index[1] > 0
 				and index[1] < grid_size[1] - 1
 			) {
-				solve_cells.push_back(cell);
+				(*cell.data)[Type()] = 1;
+			} else {
+				(*cell.data)[Type()] = 0;
 			}
 		}
 
 		pamhd::divergence::get_divergence(
-			solve_cells,
+			grid.local_cells,
 			grid,
-			[](Cell& cell_data) -> Vector_Field::data_type& {
-				return cell_data[Vector_Field()];
+			[](Cell& cell_data) -> Vector::data_type& {
+				return cell_data[Vector()];
 			},
 			[](Cell& cell_data) -> Divergence::data_type& {
 				return cell_data[Divergence()];
+			},
+			[](Cell& cell_data) -> bool {
+				return cell_data[Type()] == 1;
 			}
 		);
 
 		const double
 			p_of_norm = 2,
-			norm = get_diff_lp_norm(solve_cells, grid, p_of_norm, cell_volume);
+			norm = get_diff_lp_norm(grid, p_of_norm, cell_volume);
 
 		if (norm > old_norm) {
 			if (grid.get_rank() == 0) {
