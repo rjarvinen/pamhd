ed46d3c18c5f59c35b31a006f4cb716a2a1c632e
Ilja <ilja.honkonen@fmi.fi>
2022-11-30 11:53:59 +0200

    Update copyright in mhd2vtk_staggered

--- tests/mhd/mhd2vtk_staggered.cpp
+++ tests/mhd/mhd2vtk_staggered.cpp
@@ -2,6 +2,7 @@
 Program for converting MHD output of PAMHD to vtk format.
 
 Copyright 2014, 2015, 2016, 2017 Ilja Honkonen
+Copyright 2022 Finnish Meteorological Institute
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
